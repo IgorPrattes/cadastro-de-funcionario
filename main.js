6b94bf07163b00d401cb65509a320990e8cb9ac3
igor <igorpratespaulino@gmail.com>
2021-01-31 18:35:07 -0300

    quase lá

--- main.js
+++ main.js
@@ -1,55 +1,19 @@
-let xhr = new XMLHttpRequest()
+let reqGit = new XMLHttpRequest()
 
-        let bloco = document.querySelector("main")
+reqGit.open("GET", `https://api.github.com/users/IgorPrattes/repos`, false)
 
-        let url = "https://api.github.com/users/zeite-endless/repos";
-
-        xhr.open('GET', url)
-
-
-        xhr.onreadystatechange = function () {
-
-            if (this.readyState == 4 && this.status == 200) {
-                let dadosJSONText = xhr.responseText
-                let dadosJSONObj = JSON.parse(dadosJSONText)
-                for (let i = 0; i < dadosJSONObj.length; i++) {
-                    bloco.innerHTML +=
-                  
-                    `<div class="demo-card-wide mdl-card mdl-shadow--2dp" style="margin: 5px; margin-left:8px; width:435px;">
-                    <h2 class="mdl-card__title-text">${dadosJSONObj[i].name}</h2>
-                    <img style="width:435px; height:200px" src="images/${dadosJSONObj[i].language}.png">
-                    <button class="mdl-button mdl-js-button mdl-button--accent" style="color: black" onclick="openurl('${dadosJSONObj[i].clone_url}')">link</button>
-                    <button class="mdl-button mdl-js-button mdl-button--accent" style="color: black" onclick="clone('git clone ${dadosJSONObj[i].html_url}')">Clone</button>
-                    <button class="mdl-button mdl-js-button mdl-button--accent" style="color: black" id="${dadosJSONObj[i].name}" onclick="abrirsobre(this)">Mais Informações</button>
-                    </div>`
-                  
-                }
-            }
+reqGit.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        const repositories = JSON.parse(this.responseText)
+        for (let i = 0; i < repositories.length; i++) {
+            document.getElementById("table").innerHTML += "<div class='container'><tr><td> Name: " + repositories[i].full_name + "</td> <td> Language: " +
+                repositories[i].language + "</td><td>" + repositories[i].html_url + "<td><a target='_blank' href='./perfilfil.html' onclick = 'funcao(" + JSON.stringify(repositories[i]) + ")'><img src='https://cdn.pixabay.com/photo/2014/09/25/19/36/question-mark-460864_960_720.png' width = 40px height = 40px </a></td><br>" + "<td><img src='./Assets/" + repositories[i].language + ".PNG' width ='60px' height='60px'></img></td></tr></div>"
         }
-        xhr.send();
+    }
+}
+reqGit.send()
 
-        function openurl(url) {
-            window.open(url)
-        }
-
-        function clone(url) {
-            const input = document.createElement("input");
-            input.value = url;
-            input.id = "input";
-            document.body.appendChild(input);
-            input.select();
-            document.execCommand("copy");
-            input.remove();
-
-        }     
-
-        function abrirsobre(sobre){
-             let arr = []
-
-             arr.push({nome: sobre.id})
-             let arrJson = JSON.stringify(arr)
-
-             localStorage.repositorio = arrJson
-
-             window.location.href = "sobre.html"
-        } 
+function funcao(repositories) {
+    localStorage["repositories"] = JSON.stringify(repositories)
+    console.log(repositories)
+}
